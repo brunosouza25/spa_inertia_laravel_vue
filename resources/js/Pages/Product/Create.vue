b35b529779437a27035f984580efc6f93dde6c2e
bsouza@eleventickets.com <brunopw25@gmail.com>
2026-02-03 21:16:56 -0300

    Saving data

--- resources/js/Pages/Product/Create.vue
+++ resources/js/Pages/Product/Create.vue
@@ -18,6 +18,12 @@ const form = useForm({
     description: ''
 })
 
+const store = () => {
+    form.post(route("products.store"), {
+        onSuccess: () => form.reset()
+    })
+}
+
 </script>
 
 <template>
@@ -37,8 +43,7 @@ const form = useForm({
         <div class="py-12">
             <div class="flex items-center justify-center">
                 <div class="relative w-full max-w-2xl max-h-full">
-                    {{ form }}
-                    <form class="relative bg-white rounded-lg shadow">
+                    <form class="relative bg-white rounded-lg shadow" @submit.prevent="store">
                         <div class="p-6 space-y-6">
                             <div class="grid grid-cols-6 gap-6">
                                 <div class="col-span-6 sm:col-span-6">
@@ -55,9 +60,8 @@ const form = useForm({
                                 <div class="col-span-6 sm:col-span-3">
                                     <label for="category_id" class="block mb-2 text-sm font-medium text-gray-900 ">Category</label>
                                     <select name="category_id" v-model="form.category_id" id="category_id" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5">
-                                        <option value="">Category 1</option>
-                                        <option value="">Category 2</option>
-                                        <option value="">Category 3</option>
+                                        <option value="">Select a category</option>
+                                        <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
                                     </select>
                                 </div>
                                 <div class="col-span-6 sm:col-span-3">
