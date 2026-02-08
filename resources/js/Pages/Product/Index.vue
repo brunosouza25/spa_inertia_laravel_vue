c1b6a535cfc81138267a84605342671b8d403aab
bsouza@eleventickets.com <brunopw25@gmail.com>
2026-02-08 12:21:43 -0300

    Adding ability to check or uncheck all checkboxes

--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -5,6 +5,8 @@ import { Head, Link, router } from '@inertiajs/vue3';
 import Pagination from "@/Components/Pagination.vue";
 import Sortable from "@/Components/Sortable.vue";
 import Checkbox from "@/Components/Checkbox.vue";
+import CheckAll from "@/Components/CheckAll.vue";
+
 
 const selectedIds = ref([])
 
@@ -54,7 +56,6 @@ const handleSearch = (event) => {
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-end pb-6">
                     <div class="relative">
-                        {{ selectedIds }}
                         <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
                             <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
@@ -72,7 +73,7 @@ const handleSearch = (event) => {
                                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 border-b">
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
-                                        <Checkbox/>
+                                        <CheckAll :rows="products.data" v-model="selectedIds"/>
                                     </th>
                                     <th scope="col" class="px-6 py-3">
                                         #
